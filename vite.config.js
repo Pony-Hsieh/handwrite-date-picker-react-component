73aadc04bbe31ae39928a64799d1b546212d628e
Pony Hsieh <lbjwin@gmail.com>
2024-03-21 14:54:24 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,15 @@ import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer({})],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [autoprefixer({})],
+      },
     },
-  },
-  base:
-    // eslint-disable-next-line
-    process.env.NODE_ENV === 'production'
-      ? '/handwrite-date-picker-react-component/'
-      : '/',
+    base:
+      mode === 'production' ? '/handwrite-date-picker-react-component/' : '/',
+  };
 });
