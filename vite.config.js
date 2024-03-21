d0a046bddc1d8bb785a2eabe46ee7af7efac8b34
Pony Hsieh <lbjwin@gmail.com>
2024-03-21 14:27:22 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
-import vitePluginRequire from 'vite-plugin-require';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [autoprefixer({})],
     },
   },
+  base:
+    // eslint-disable-next-line
+    process.env.NODE_ENV === 'production'
+      ? '/handwrite-date-picker-react-component/'
+      : '/',
 });
